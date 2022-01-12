3d8f9c28471f69e8276823d9cffd0f9e641284e2
Jerry Tong <zihangtong@gmail.com>
2022-01-12 18:47:50 -0500

    commit tree d65761da0deb21c5d6ce3704af6f0acd1af7fea2

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+require 'pry'
+class CashRegister
+    attr_accessor :total, :previous_transaction
+    attr_reader :discount, :items_array
 
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items_array = []
+    end
+
+    def add_item(title, price, quantity=1)
+        quantity.times do
+            @items_array.push({title: title, price: price, quantity: quantity})
+        end
+        @total += price * quantity
+        @previous_transaction = price * quantity
+    end
+
+    def apply_discount
+        if @discount > 0
+            @total *= (1 - @discount/100.0)
+            return "After the discount, the total comes to $#{@total.to_i}."
+        else 
+            return "There is no discount to apply."
+        end
+    end
+
+    def items
+        self.items_array.map do |i|
+            item[:title]
+        end
+    end
+
+    def void_last_transaction 
+        self.total -= @previous_transaction
+        @previous_transaction = 0
+    end
+end
